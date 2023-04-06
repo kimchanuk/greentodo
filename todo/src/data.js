cccbebd1503b49fd1abf9c2b503d557749b11ba1
kimchanuk <121743865+kimchanuk@users.noreply.github.com>
2023-04-06 14:16:23 +0900

    gg
    gg

---
+++ todo/src/data.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const data = [
+  {
+    id: 1,
+    text: '할일이 이곳에 표시됩니다',
+    checked: false,
+  },
+  {
+    id: 2,
+    text: '목록을 클릭하면 수정과 삭제를 하실수 있습니다',
+    checked: false,
+  },
+  {
+    id: 3,
+    text: '오른쪽 아래 플러스 버튼을 클릭하면 목록을 추가하실 수 있습니다',
+    checked: false,
+  },
+  {
+    id: 4,
+    text: '회색 배경화면을 클릭하면 메인페이지로 돌아갑니다',
+    checked: false,
+  },
+];
+
+export default data;
